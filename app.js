193415f6e970b81f7d36a5d6b23c516597e86986
dikshitakambri <dikshitakambri18@gmail.com>
2021-02-01 11:51:43 +0530

    mongoose db

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
-var items = ["1","2","3"];
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost:27017/todolistDB" ,{ useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB" ,{ useNewUrlParser: true , useUnifiedTopology: true});
 
-// const itemSchema = {
-//     name : String
-// };
+var today = new Date();
+    var currentday = today.getDay();
+    var options = {
+        month:"long",
+        year : "numeric",
+        weekday :"long"
+    };
 
-// const Item = mongoose.model("Item", itemSchema);
+var day = today.toLocaleDateString("en-US", options);
 
-// const item1 = new Item ({
-//     name : "Work 1"
-// });
 
-// const item2 = new Item ({
-//     name : "Work 2"
-// });
+const itemsSchema = {
+    name : String
+};
 
-// const defaultList = [item1 , item2];
+const Item = mongoose.model("Item", itemsSchema);
 
+const item1 = new Item ({
+    name : "Work 1"
+});
+
+const item2 = new Item ({
+    name : "Work 2"
+});
+
+const defaultItems = [item1 , item2];
+
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Successfully aaded");
+    }
+});
 // const listSchema = {
 //     name : String,
 //     item : [itemSchema]
@@ -35,17 +52,8 @@ app.use(express.static("public"));
 // const List = mongoose.model("List", listSchema);
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var currentday = today.getDay();
-    var options = {
-        month:"long",
-        year : "numeric",
-        weekday :"long"
-    };
 
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay : day, newListItems : items});
+    res.render("list", {ListTitle : day, newListItems : defaultItems});
 });
 
 //     Item.find({}, function(err, foundItems){
