4361dfbdf74eaf1da55cae938d983b2fd1b08dd5
dikshitakambri <dikshitakambri18@gmail.com>
2021-02-02 19:26:03 +0530

    heroku deployment

--- app.js
+++ app.js
@@ -136,7 +136,12 @@ app.get("/:customListName", function(req, res){
     
  });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server running on 3000 port");
 });
 
